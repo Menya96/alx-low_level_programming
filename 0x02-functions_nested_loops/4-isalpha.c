1daef5c961a2eb3319e6b5831794567bf917b826
Menya96 <menyahtonny@gmail.com>
2022-09-14 21:02:16 +0300

    task 4 isalpha function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isalpha - function that checks for alphabetic character
+* @c: char type letter
+* Return: return 0 if successful and 1 if failed
+*/
+
+init _isalpha(int c)
+{
+init i = _isalpha(c);
+
+if (i > 0)
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
