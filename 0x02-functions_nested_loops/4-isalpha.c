6025fb4364b8dca8dd32dc907f1873490589da31
Menya96 <menyahtonny@gmail.com>
2022-09-14 21:43:03 +0300

    fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 * _isalpha - function that checks for alphabetic character
 * @c: char type letter
@@ -9,5 +8,5 @@
 
 int _isalpha(int c)
 {
-return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
