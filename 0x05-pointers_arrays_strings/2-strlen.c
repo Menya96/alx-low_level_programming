2a3825a574c06f3f7368c7932850b189f81b0ad8
Menya96 <menyahtonny@gmail.com>
2022-09-20 20:54:23 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen -> gets length of a string
+@s: string pointer
+*Return: returns length of a string
+*/
+int _strlen(char *s)
+{
+    int len;
+
+    for(; *s != '\0'; s++)
+    {
+        len += 1;
+    }
+    return (len);
+}
