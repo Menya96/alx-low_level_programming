de5a41ceed389156d2a0ac1a249e868a2df5243d
Menya96 <menyahtonny@gmail.com>
2022-09-20 22:47:46 +0300

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 #include <string.h>
 
 /**
@@ -11,7 +11,7 @@ void puts2(char *str)
 int len, i;
 
 len = strlen(str);
-for (i = 0; i < len; i += 2)
+for (i = 0; i < len; i +=  2)
 _putchar(str[i]);
 _putchar('\n');
 }
