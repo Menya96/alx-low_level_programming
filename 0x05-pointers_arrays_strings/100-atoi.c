cc432e0eb5ade788222be48b68c43d3635ce7e24
Menya96 <menyahtonny@gmail.com>
2022-09-20 23:30:05 +0300

    advanced task 1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,14 +22,14 @@ min *= -1;
 
 while (s[c] >= 48 && s[c] <= 57)
 {
-    isi = 1;
-    ni = (ni * 10) + (s[c] - '0');
-    c++;
+isi = 1;
+ni = (ni * 10) + (s[c] - '0');
+c++;
 }
 
 if (isi == 1)
 {
-    break;
+break;
 }
 
 c++;
