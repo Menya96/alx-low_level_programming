86ce45575f65745b8422909a31a5411614f247e3
Menya96 <menyahtonny@gmail.com>
2022-09-24 14:18:45 +0300

    issue resolved

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,18 +7,14 @@
  */
 int main(void)
 {
-int ch;
-
-for (ch = 97; ch <= 122; ch++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
 {
 putchar(ch);
 }
-for (ch = 65; ch <= 90; ch++)
-{
-putchar(ch);
 }
-putchar(10); /* this is an ascii code for new line*/
-
+putchar(10); 
 return (0);
 }
- 
