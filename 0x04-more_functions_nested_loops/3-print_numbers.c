691049fb50327cd74219ada67d273ad2cc815ef0
Menya96 <menyahtonny@gmail.com>
2023-01-03 22:20:45 +0300

    fixes before the game

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_numbers(void)
 {
-	char c = '0';
+char c = '0';
 
-	while (c <= '9')
-    
-	{
-		_putchar(c);
-		c++;
-	}
+while (c <= '9')
+
+{
+_putchar(c);
+c++;
+}
 _putchar('\n');
 }
