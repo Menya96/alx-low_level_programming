510988829b7f7d694ce392908b17cb116248763b
Menya96 <menyahtonny@gmail.com>
2024-09-12 06:05:59 -0700

    solution for task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+
 /**
- * main -  program that finds and prints the largest prime factor of a number
- * Return: Always 0.
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
+ * Return: 0
  */
 
 int main(void)
 {
-long int num = 612852475143;
-int prime;
+	int c;
+	long num = 612852475143;
 
-for (prime = 2; prime <= sqrt(num); prime++)
-{
-    /*int saved_prime;*/
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
 
-    if (num % prime == 0)
-    {
-        /*saved_prime = prime;*/
-        num = num / prime;
-        prime = 1;
-    /* printf("%ld\n", num);*/
-    }
-}
-printf("%ld\n", num);
-return (0);
+	return (0);
 }
